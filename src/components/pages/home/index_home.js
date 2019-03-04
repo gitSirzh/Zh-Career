de611b8d8ae0af5a9a3283941c042b1fec908005
jszh <969883825@qq.com>
2019-03-04 15:34:19 +0800

    🎉🎉 device info

--- src/components/pages/home/index_home.js
+++ src/components/pages/home/index_home.js
@@ -5,8 +5,6 @@ import React, {Component} from 'react'
 import {StyleSheet, View,Text} from 'react-native'
 import {commonStyle} from "../../../utils/commonStyle";
 import {BaseComponent} from "../../base/baseComponent";
-import deviceInfo from "../../../utils/deviceInfo";
-import DeviceInfo from 'react-native-device-info'
 
 class Home extends BaseComponent {
 
@@ -33,18 +31,16 @@ class Home extends BaseComponent {
 
     }
 
+    /**
+     * 后面加入swiper组件介绍
+     * 后期准备加入新的东西，有意思的东西呦
+     * @returns {*}
+     * @private
+     */
     _render() {
         return (
             <View style={styles.containerStyle}>
                 <Text>Home</Text>
-                <Text>{'获取API级别:'}{deviceInfo.getAPILevel}</Text>
-                <Text>{'获取应用程序名称:'}{deviceInfo.getApplicationName}</Text>
-                <Text>{'获取应用程序包名:'}{deviceInfo.getBundleId}</Text>
-                <Text>{'获取设备名称:'}{deviceInfo.getDeviceName}</Text>
-                <Text>{'获取设备系统名称:'}{deviceInfo.getSystemName}</Text>
-                <Text>{'获取设备系统版本号:'}{deviceInfo.getSystemVersion}</Text>
-                <Text>{'获取设备系统:'}{deviceInfo.getSystemNameAndVersion}</Text>
-                <Text>{'设备总内存:'}{deviceInfo.getTotalMemory}</Text>
             </View>
         )
     }
