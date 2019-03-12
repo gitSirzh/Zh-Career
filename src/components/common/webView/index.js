680e577fff3be86cad5a409f1ead164ddf6ec5dc
jszh <969883825@qq.com>
2019-03-12 10:07:08 +0800

    goBack

--- src/components/common/webView/index.js
+++ src/components/common/webView/index.js
@@ -2,49 +2,54 @@
  * Created by jszh on 2018/12/29.
  */
 import React, {Component} from 'react'
-import {StyleSheet, View, WebView} from 'react-native'
+import {StyleSheet, View, WebView,TouchableOpacity} from 'react-native'
 import {commonStyle} from '../../../utils/commonStyle'
 import {BaseComponent} from '../../base/baseComponent'
+
 export default class CustomWebView extends BaseComponent {
 
-  navigationBarProps() {
-    return {
-      title: this.props.title,
-      subTitleStyle: {
-        color: commonStyle.white
-      },
-      titleStyle: {
-        color: commonStyle.white
-      },
-      leftIcon: {
-        name: 'ios-arrow-back',
-        size: 26,
-        color: commonStyle.white
-      },
-      navBarStyle: {
-        backgroundColor: '#151C28',
-      }
+    navigationBarProps() {
+        return {
+            title: this.props.title,
+            subTitleStyle: {
+                color: commonStyle.white
+            },
+            titleStyle: {
+                color: commonStyle.white
+            },
+            leftIcon: {
+                name: 'ios-arrow-back',
+                size: 26,
+                color: commonStyle.white
+            },
+            navBarStyle: {
+                backgroundColor: '#151C28',
+            }
+        }
     }
-  }
 
-  _render() {
-    const {url} = this.props;
-    return (
-      <View style={styles.container}>
-        <WebView
-          ref={'webview'}
-          startInLoadingState={true}
-          source={{ uri: url}}
-          userAgent={'userAgent'}
-        />
-      </View>
-    )
-  }
+    _render() {
+        const {url} = this.props;
+        return (
+            <View style={styles.container}>
+                {/*<TouchableOpacity*/}
+                    {/*style={{width:200,height:40,backgroundColor:'red'}}*/}
+                    {/*onPress={()=>{this.refs['webView'].goBack()}}*/}
+                {/*/>*/}
+                <WebView
+                    ref={'webView'}
+                    startInLoadingState={true}
+                    source={{uri: url}}
+                    userAgent={'userAgent'}
+                />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: commonStyle.white
-  }
+    container: {
+        flex: 1,
+        backgroundColor: commonStyle.white
+    }
 })
